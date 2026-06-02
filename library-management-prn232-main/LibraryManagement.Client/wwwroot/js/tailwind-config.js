c0918be3451563678babbae6b07cf0d5aec6148c
Hiep204 <nguyenhiep762004@gmail.com>
2026-06-02 20:58:17 +0700

    Form structure system
    Check luôn nhé

---
+++ library-management-prn232-main/LibraryManagement.Client/wwwroot/js/tailwind-config.js
@@ -0,0 +1,23 @@
+tailwind.config = {
+    darkMode: "class",
+    theme: {
+        extend: {
+            colors: {
+                "primary": "#f2df0d",
+                "background-light": "#f8f8f5",
+                "background-dark": "#0F2E2E",
+                "teal-deep": "#081a1a",
+                // Additional colors if needed
+            },
+            fontFamily: {
+                "display": ["Inter", "sans-serif"]
+            },
+            borderRadius: {
+                "DEFAULT": "0.5rem",
+                "lg": "1rem",
+                "xl": "1.5rem",
+                "full": "9999px"
+            },
+        },
+    },
+};
---
+++ library-management-prn232-main/LibraryManagement.DAL/Models/BookReview.cs
@@ -0,0 +1,15 @@
+﻿using LibraryManagementDAL.Models;
+
+public class BookReview : BaseEntity
+{
+    public int BookReviewId { get; set; }
+    public int UserId { get; set; }
+    public int BookId { get; set; }
+
+    public int Rating { get; set; }   // 1 -> 5
+    public string? Comment { get; set; }
+
+    // Navigation Properties
+    public User User { get; set; }
+    public Book Book { get; set; }
+}
